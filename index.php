70a5566423077024b9a60cdb5421bd4528dfb06c
tiepolo <tiepolo71@gmail.com>
2023-10-15 16:08:38 -0700

    Updated some stying and formattting.

--- index.php
+++ index.php
@@ -17,6 +17,18 @@
             <label for="email" class="form-label">Email:</label>
             <input type="text" name="email" id="email" class="form-control" />
         </div>
+        <div class="col-auto">
+            <label for="city" class="form-label">City:</label>
+            <input type="text" name="city" id="city" class="form-control" />
+        </div>
+        <div class="col-auto">
+            <label for="state" class="form-label">State:</label>
+            <input type="text" name="state" id="state" class="form-control" />
+        </div>
+        <div class="col-auto">
+            <label for="country" class="form-label">Country:</label>
+            <input type="text" name="country" id="country" class="form-control" />
+        </div>        
         <div class="row submit-btn">
             <div class="col-auto">
                 <input type="submit" value="Search" class="btn btn-primary" />
@@ -44,19 +56,27 @@
         // Process the form data
         $name = $_POST["name"];
         $email = $_POST["email"];
+        $city = $_POST["city"];
+        $state = $_POST["state"];
+        $country = $_POST["country"];
 
         // Log form data to the PHP error log
-        error_log("Name: $name, Email: $email");
+        error_log("Name: $name, Email: $email, City: $city, State: $state, Country: $country");
 
         // Create and execute a SQL query to search for results that match both fields
-        $sql = "SELECT full_name, email, display_name, dob, address_first, city, state, zipcode, country, phone FROM users WHERE full_name LIKE '%$name%' AND email LIKE '%$email%'";
+        $sql = "SELECT full_name, email, display_name, dob, address_first, city, state, zipcode, country, phone FROM users 
+        WHERE full_name LIKE '%$name%' 
+        AND email LIKE '%$email%'
+        AND city LIKE '%$city%'
+        AND state LIKE '%$state%'
+        AND country LIKE '%$country'";
 
         $result = $conn->query($sql);
 
         // Display search results and count
         if ($result->num_rows > 0) {
-            echo '<h2>Search Results:</h2>';
-            echo '<h3><strong class="text-danger">' . $result->num_rows . "</strong> results found for \"$name\", \"$email\"</h3>"; // Display the count of results
+            echo '<h2><strong>Search Results:</strong></h2>';
+            echo '<h3><strong class="text-danger">' . $result->num_rows . "</strong> results found for <strong>Name:</strong> \"$name\", <strong>Email:</strong> \"$email\", <strong>City:</strong> \"$city\", <strong>State:</strong> \"$state\", <strong>Country:</strong> \"$country\" </h3>"; // Display the count of results
             while ($row = $result->fetch_assoc()) {
                 echo '<div class="card">';
                 echo '<div class="card-header custom-purple">' . $row['full_name'] . '</div>';
