57d07d8c8618fb14d2d9ae7fa575de2d35785cab
tiepolo <tiepolo@sbcglobal.net>
2023-11-09 08:59:24 -0800

    Added pagination

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 </head>
 <body>
     <h1>Member Search</h1>
-    <form method="post" action="" class="row gy-2 gx-3 align-items-center">
+    <form method="get" action="" class="row gy-2 gx-3 align-items-center">
         <div class="row my-5">
             <div class="col-auto">
                 <label for="name" class="form-label">Name:</label>
@@ -50,13 +50,12 @@
 
     <?php    
     // Check if the form was submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
         // Connect to the MySQL database
         $servername = "localhost"; // Change this to your MySQL server address
         $username = "root"; // Change this to your MySQL username
         $password = ""; // Set the password to an empty string
         $database = "rhs"; // Change this to your MySQL database name
-
         $conn = new mysqli($servername, $username, $password, $database);
 
         // Check the database connection
@@ -65,28 +64,45 @@
         }
 
         // Process the form data
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $city = $_POST["city"];
-        $state = $_POST["state"];
-        $country = $_POST["country"];
+        $name = $_GET["name"] ?? '';
+        $email = $_GET["email"] ?? '';
+        $city = $_GET["city"] ?? '';
+        $state = $_GET["state"] ?? '';
+        $country = $_GET["country"] ?? '';
+
+        // Pagination variables
+        $itemsPerPage = 10; // Define how many items per page
+        $page = $_GET['page'] ?? 1; // Get the current page number from the URL
+        $offset = ($page - 1) * $itemsPerPage; // Calculate the offset        
+
+        // Calculate total number of pages
+        $totalSql = "SELECT COUNT(DISTINCT u.mtt_id) AS total
+                    FROM users u
+                    WHERE u.full_name LIKE '%$name%'
+                    AND u.email LIKE '%$email%'
+                    AND u.city LIKE '%$city%'
+                    AND u.state LIKE '%$state%'
+                    AND u.country LIKE '%$country%'";
+
+        $totalSql = "SELECT COUNT(DISTINCT u.mtt_id) AS total FROM users u WHERE u.full_name LIKE '%$name%' AND u.email LIKE '%$email%' AND u.city LIKE '%$city%' AND u.state LIKE '%$state%' AND u.country LIKE '%$country%'";
+        $totalResult = $conn->query($totalSql);
+        $totalRow = $totalResult->fetch_assoc();
+        $totalItems = $totalRow['total'];
+        $totalPages = ceil($totalItems / $itemsPerPage);       
 
         // Log form data to the PHP error log
         error_log("Name: $name, Email: $email, City: $city, State: $state, Country: $country");
 
         // Create and execute a SQL query to search for results that match the fields and join with the legacy_ids table
-        $sql = "SELECT u.full_name, u.email, u.display_name, u.dob, u.address_first, u.city, u.state, u.zipcode, u.country, u.phone, l.legacy_id, u.membership_type, MAX(m.membership_end_at) AS latest_membership_end_at, GROUP_CONCAT(DISTINCT CONCAT(cm.role, ':', c.chapter_name) SEPARATOR ',') AS chapter_data
-                FROM users u
-                INNER JOIN legacy_ids l ON u.mtt_id = l.mtt_id
-                LEFT JOIN chapter_member cm ON u.mtt_id = cm.author_id
-                LEFT JOIN chapters c ON cm.chapter_id = c.mtt_chapter_id
-                LEFT JOIN membership m ON u.mtt_id = m.mtt_id
-                WHERE u.full_name LIKE '%$name'
-                AND u.email LIKE '%$email'
-                AND u.city LIKE '%$city'
-                AND u.state LIKE '%$state'
-                AND u.country LIKE '%$country'
-                GROUP BY u.mtt_id";
+        $sql = "SELECT u.full_name, u.email, u.display_name, u.dob, u.address_first, u.city, u.state, u.zipcode, u.country, u.phone, l.legacy_id, u.membership_type, MAX(m.membership_end_at) AS latest_membership_end_at, GROUP_CONCAT(DISTINCT CONCAT(cm.role, ':', c.chapter_name) SEPARATOR ',') 
+                AS chapter_data 
+                FROM users u 
+                INNER JOIN legacy_ids l ON u.mtt_id = l.mtt_id 
+                LEFT JOIN chapter_member cm ON u.mtt_id = cm.author_id 
+                LEFT JOIN chapters c ON cm.chapter_id = c.mtt_chapter_id 
+                LEFT JOIN membership m ON u.mtt_id = m.mtt_id 
+                WHERE u.full_name LIKE '%$name%' AND u.email LIKE '%$email%' AND u.city LIKE '%$city%' AND u.state LIKE '%$state%' AND u.country LIKE '%$country%' 
+                GROUP BY u.mtt_id LIMIT $itemsPerPage OFFSET $offset";
 
         // Execute the query
         $result = $conn->query($sql);
@@ -120,7 +136,8 @@
                 echo '</h3>';
                     while ($row = $result->fetch_assoc()) {
                     $crownIcon = ($row['membership_type'] === 'Queen') ? '<i class="fa-solid fa-crown"></i> ' : '';
-                    echo '<span class="fs-5">' . $crownIcon . '<strong>' . $row['full_name'] . '</strong></span><br />' . '<span class="fs-sm text-secondary">' . $row['legacy_id'] . ' | <span class="fw-medium"><strong class="text-uppercase ls-wider">Expiration Date:</strong> ' . 
+                    echo '<div class="mb-10">';
+                    echo '<span class="fs-5">' . $crownIcon . '<strong>' . $row['full_name'] . '</strong></span><br />' . '<span class="fs-sm text-secondary">' . $row['legacy_id'] . ' | <span class="fw-medium"><strong class="text-primary ls-wider">Exp:</strong> ' . 
                     date('F d, Y', strtotime($row['latest_membership_end_at'])) . '</span><br /><br />';
                     echo '<p><strong>Email: </strong>' . $row['email'] . '</p>';
                     echo '<p><strong>Display Name: </strong>' . $row['display_name'] . '</p>';
@@ -159,7 +176,8 @@
                         }
                     
                         echo '</p>';
-                    }                    
+                    } 
+                    echo '</div>';                   
                 }
             } else {
                 echo "<h2>No results found.</h2>";
@@ -168,7 +186,47 @@
             // Close the database connection
             $conn->close();
         }
+
+        // Collect search parameters
+        $searchParams = [
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+            'country' => $country,
+            // include any other parameters here
+        ];
+
+        // Start the pagination
+        echo '<nav aria-label="Page navigation example">';
+        echo '<ul class="pagination">';
+
+        // Previous button
+        $disabledClass = ($page <= 1) ? ' disabled' : '';
+        $searchParams['page'] = max(1, $page - 1); // Update page number for the Previous button
+        $queryString = http_build_query($searchParams);
+        echo "<li class='page-item{$disabledClass}'><a class='page-link' href='?$queryString'>Previous</a></li>";
+
+        // Page number buttons
+        for ($i = 1; $i <= $totalPages; $i++) {
+            $activeClass = ($page == $i) ? ' active' : '';
+            $searchParams['page'] = $i; // Update page number for each page button
+            $queryString = http_build_query($searchParams);
+            echo "<li class='page-item{$activeClass}'><a class='page-link' href='?$queryString'>{$i}</a></li>";
+        }
+
+        // Next button
+        $disabledClass = ($page >= $totalPages) ? ' disabled' : '';
+        $searchParams['page'] = min($totalPages, $page + 1); // Update page number for the Next button
+        $queryString = http_build_query($searchParams);
+        echo "<li class='page-item{$disabledClass}'><a class='page-link' href='?$queryString'>Next</a></li>";
+
+        // End the pagination
+        echo '</ul>';
+        echo '</nav>';      
+
     ?>
+
     <script src="https://cdn.jsdelivr.net/npm/fastbootstrap@2.0.0/dist/js/fastbootstrap.min.js" integrity="sha256-o0tNXN7ia0O9G0qNbrzBkEEiQTv+GeW5EO4LjnfDkZk=" crossorigin="anonymous"></script>
 </body>
 </html>
